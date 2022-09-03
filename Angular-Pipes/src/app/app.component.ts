220a49997577bedf2a00e36c13444bd6a2950cf8
Lyubomir Lyubomirov <Ljubo6@abv.bg>
2022-09-03 13:58:33 +0300

    add

--- Angular-Pipes/src/app/app.component.ts
+++ Angular-Pipes/src/app/app.component.ts
@@ -1,23 +1,47 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from "rxjs";
+export interface Post {
+  title: string,
+  text: string
+}
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  e: number = Math.E
-  str: string = 'hello world'
-  date: Date = new Date()
-  float: number = 0.42
-  obj:Object = {
-    a: 1,
-    b:{
-      c:2,
-      d:{
-        e:3,
-        f:4
-      }
-    }
+export class AppComponent implements OnInit{
+
+  p: Promise<string> = new Promise<string>(resolve => {
+    setTimeout(() => {
+      resolve('Promise resolved')
+    },4000)
+  })
+  date$: Observable<Date> = new Observable(obs => {
+    setInterval(() => {
+      obs.next(new Date())
+    },1000)
+  })
+  date!: Date
+
+  search = ''
+  searchField = 'title'
+  posts: Post[] = [
+    {title:'Beer',text:'The best drink in the world'},
+    {title:'Bread',text:'The best food in the world'},
+    {title:'Javascript',text:'The best language in the world'}
+  ]
+
+  addPost() {
+    this.posts.unshift({
+      title: 'Angular',
+      text:'Lyubomir course'
+    })
+  }
+  ngOnInit() {
+    this.date$.subscribe(date => {
+      this.date = date
+    })
   }
 }
--- Angular-Pipes/src/app/app.module.ts
+++ Angular-Pipes/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
 import {MultByPipe} from "./pipes/mult-by.pipe";
+import { ExMarksPipe } from './pipes/ex-marks.pipe';
+import { FilterPipe } from './pipes/filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MultByPipe
+    MultByPipe,
+    ExMarksPipe,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
