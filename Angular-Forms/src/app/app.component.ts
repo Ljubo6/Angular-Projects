89570de8136ec7d6d16b385d6c9f3d842f2189ad
Lyubomir Lyubomirov <Ljubo6@abv.bg>
2022-09-05 00:08:34 +0300

    init

--- Angular-Forms/src/app/app.component.ts
+++ Angular-Forms/src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Angular-Forms';
+export class AppComponent implements OnInit{
+  form!: FormGroup
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl(null,[
+        Validators.required,
+        Validators.email
+      ]),
+      password: new FormControl(null,[
+        Validators.required,
+        Validators.minLength(6)
+      ]),
+      address: new FormGroup({
+        country: new FormControl('bg'),
+        city: new FormControl('Sofia',Validators.required)
+      })
+    })
+  }
+
+  submit() {
+    if(this.form.valid){
+      console.log('Form: ',this.form)
+      const formData = {...this.form.value}
+      console.log('Form data: ',formData)
+    }
+
+  }
+
+  setCapital() {
+    const cityMap:any = {
+      ru: 'Moscow',
+      bg:'Sofia',
+      rs:'Beograd'
+    }
+    let cityKey = this.form.get('address')!.get('country')!.value
+    let city= cityMap[cityKey]
+    this.form.patchValue({
+      address:{city: city}
+    })
+  }
 }
