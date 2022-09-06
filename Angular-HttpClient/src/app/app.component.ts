c4a4d45997be2766e88dfec22ab10b350e76586f
Lyubomir Lyubomirov <Ljubo6@abv.bg>
2022-09-07 00:01:36 +0300

    init

--- Angular-HttpClient/src/app/app.component.ts
+++ Angular-HttpClient/src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Todo, TodosService} from "./todos.service";
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Angular-HttpClient';
+export class AppComponent implements OnInit{
+  todos:Todo[] = []
+  loading = false
+  todoTitle = ''
+
+  constructor(private todosService: TodosService) {
+
+  }
+
+
+  ngOnInit() {
+    this.fetchTodos()
+  }
+
+  addTodo() {
+    if(!this.todoTitle.trim()){
+      return
+    }
+
+    this.todosService.addTodo({
+      title: this.todoTitle,
+      completed: false
+    }).subscribe(todo => {
+      console.log('todo',todo)
+      this.todos.push(todo)
+      this.todoTitle = ''
+    })
+
+  }
+  fetchTodos(){
+    this.loading = true
+    this.todosService.fetchTodos()
+      .subscribe(todos => {
+        console.log(todos)
+        this.todos = todos
+        this.loading = false
+      })
+  }
+
+  removeTodo(id: number) {
+    this.todosService.removeTodo(id)
+      .subscribe(() => {
+        this.todos = this.todos.filter(t => t.id !== id)
+      })
+  }
+
+  completeTodo(id: number) {
+    this.todosService.completeTodo(id).subscribe(todo => {
+      this.todos.find(t => t.id === todo.id)!.completed = true
+    })
+  }
 }
