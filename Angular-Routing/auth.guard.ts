06a7a2f4b4a77da96413b6e912a1255efaba2e6c
Lyubomir Lyubomirov <Ljubo6@abv.bg>
2022-09-13 10:29:49 +0300

    init s

--- Angular-Routing/auth.guard.ts
+++ Angular-Routing/auth.guard.ts
@@ -1,13 +1,30 @@
-import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from "@angular/router";
+import {ActivatedRouteSnapshot, CanActivate,Router, RouterStateSnapshot} from "@angular/router";
 import {Observable} from "rxjs";
 import {Injectable} from "@angular/core";
+import {AuthService} from "./src/app/auth.service";
+
 @Injectable({providedIn:"root"})
 export class AuthGuard implements CanActivate{
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {
+  }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ):Observable<boolean> | Promise<boolean> | boolean{
 
-    return null
+    return this.authService.isAuthenticated().then( (isAuth: boolean) => {
+      if(isAuth){
+        return true
+      }else{
+        return this.router.navigate(['/'],{
+          queryParams:{
+            auth: false
+          }
+        })
+      }
+    })
   }
 }
