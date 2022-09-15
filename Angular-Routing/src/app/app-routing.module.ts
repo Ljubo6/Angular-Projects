288bc5d5cbb5f14533964be4a2ff23aef50bde42
Lyubomir Lyubomirov <Ljubo6@abv.bg>
2022-09-15 23:38:40 +0300

    init home

--- Angular-Routing/src/app/app-routing.module.ts
+++ Angular-Routing/src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import {PostsComponent} from "./posts/posts.component";
 import {PostComponent} from "./post/post.component";
 import {AboutExtraComponent} from "./about-extra/about-extra.component";
 import {ErrorPageComponent} from "./error-page/error-page.component";
+import {AuthGuard} from "./auth.guard";
+import {PostResolver} from "./post.resolver";
 
 //http://localhost:4200/ -> HomeComponent
 //http://localhost:4200/about -> AboutComponent
@@ -14,11 +16,17 @@ import {ErrorPageComponent} from "./error-page/error-page.component";
 
 const routes: Routes = [
   {path:'',component: HomeComponent},
-  {path:'about',component: AboutComponent,children:[
+  {path:'about',component: AboutComponent,canActivateChild:[AuthGuard],children:[
       {path: 'extra',component:AboutExtraComponent}
     ]},
-  {path:'posts',component:PostsComponent},
-  {path:'posts/:id',component:PostComponent},
+  {path:'posts',component:PostsComponent,canActivate:[AuthGuard]},
+  {
+    path:'posts/:id',
+    component:PostComponent,
+    resolve:{
+      post:PostResolver
+    }
+  },
   {path:'error',component:ErrorPageComponent},
   {path: '**',redirectTo: '/error'}
 ];
---
+++ Angular-Routing/src/app/post.resolver.ts
@@ -0,0 +1,21 @@
+
+import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from "@angular/router";
+import {delay, Observable, of} from "rxjs";
+import {Post, PostsService} from "./posts.service";
+import {Injectable} from "@angular/core";
+
+@Injectable({providedIn: 'root'})
+export class PostResolver implements Resolve<Post> {
+  constructor(private postService: PostsService) {
+
+  }
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): any {
+
+    return of (this.postService.getById(Number(route.params['id'])))
+      .pipe(delay(1500))
+
+  }
+
+}
